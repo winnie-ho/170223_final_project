6f729e155fc2b5283c64c122d1339abe4bd53935
Winnie Ho <winnie-ho@hotmail.co.uk>
2017-02-25 00:33:25 +0000

    got a message to create to board and tested with insomnia

--- boardApp/app/controllers/messages_controller.rb
+++ boardApp/app/controllers/messages_controller.rb
@@ -1,14 +1,13 @@
 class MessagesController < ApplicationController
 
   def messages_params()
-    params.require(:message).permit([:msg])
+    params.require(:message).permit([:msg, :group_id])
   end
 
   def index()
     messages = Message.all()
     render({:json => messages})
   end
-
 
   def create()
     message = Message.create(messages_params())
