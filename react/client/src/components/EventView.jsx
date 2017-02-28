cddd52b7b64f4da53aab4aa017608d648e4e87c7
Winnie Ho <winnie-ho@hotmail.co.uk>
2017-02-28 17:12:42 +0000

    can delete an event

--- react/client/src/components/EventView.jsx
+++ react/client/src/components/EventView.jsx
@@ -4,13 +4,35 @@ import { Link, browserHistory, hashHistory } from "react-router";
 class EventView extends React.Component{
   constructor(props){
     super(props)
+    this.goBack = this.goBack.bind(this);
+    this.deleteEvent = this.deleteEvent.bind(this);
     console.log(this.props.location.query)
 
 
   }
 
   goBack(){
-  browserHistory.goBack();
+    browserHistory.goBack();
+  }
+
+  deleteEvent(){
+    event.preventDefault();
+    var eventId = this.props.location.query.id
+    var url = "http://localhost:5000/groups/:id/events/" + eventId + ".json"
+
+
+    const request = new XMLHttpRequest();
+    request.open("DELETE", url);
+    request.setRequestHeader("content-type", "application/json");
+    request.withCredentials = true;
+
+    request.onload = ()=>{
+      if(request.status === 201){
+        console.log("event deleted", data);
+      }
+        this.goBack()
+    }
+    request.send()
   }
 
 
@@ -23,7 +45,7 @@ class EventView extends React.Component{
           <div onClick = {this.goBack}>←back</div>
 
           <div className = "top-bar-right">
-          <button className = "icon-button">✄</button>
+          <button onClick = {this.deleteEvent} className = "icon-button">✄</button>
           <button className = "icon-button">✎</button>
           </div>
         </div>
